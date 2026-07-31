d4b93446e6479d774ec855223b02afda08022484
Solomiia Kornaha <113928269+Solichka27@users.noreply.github.com>
2026-07-31 17:20:47 +0300

    initializationProject

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Layout from "./layout/layout/Layout";
+
+
+function App() {
+  return <Layout/>;
+}
+
+export default App;
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,9 @@
+function Home (){
+    return(
+        <main>
+            <h1>Welcome to BeatFlow 🎵</h1>
+        </main>
+    );
+}
+
+export default Home;
